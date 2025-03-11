3d6162019b4da912b074af6e478ac0dcc265d77d
aisha <caishaabdlla@gmail.com>
2025-03-11 15:29:03 +0300

    update

--- models/favoriteModel.js
+++ models/favoriteModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const FavoriteSchema = new mongoose.Schema({
     id: {
-        type: Number,
+        type: String,
         required: true,
     },
     name: {
